a87184a3578f85bfda4af1c72d1049a865a307a1
azg <aristarkhov.zakhar-2003@mail.ru>
2022-11-17 01:11:48 +0300

    17.11

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,7 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'Zakhar': 'Zakhar2003',
     },
 ]
 
